450d0d635e2e7e1cc533b5c5c77f163882e9c196
Aasif Azlan <aasifazlaan@gmail.com>
2025-05-19 13:45:47 +0530

    apiRouteFixed

--- src/stores/useUserStore.jsx
+++ src/stores/useUserStore.jsx
@@ -26,7 +26,7 @@ export const useUserStore=create((set, get)=> ({
     login: async({email, password})=>{
         set({loading: true})
         try {
-            const res= await axios.post("/api/auth/login", {email, password})
+            const res= await axios.post("/auth/login", {email, password})
             console.log(res)
             console.log(res.data)
             set({user:res.data, loading: false})
