51a0200eefc395bea7ce130d52a5e683fd8ed3d7
Aasif Azlan <aasifazlaan@gmail.com>
2025-05-19 13:43:53 +0530

    userRouteIssue

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.DEV
 		? "http://localhost:5000/api"
-		: "https://mern-ecom-backend-oay9.onrender.com/api",
+		: "https://mern-ecom-backend-oay9.onrender.com/",
 	withCredentials: true,
 });
 
