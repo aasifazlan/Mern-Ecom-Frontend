b0d2156f39024108ed2f138e3f67e710439f8632
Aasif Azlan <136929228+aasifazlan@users.noreply.github.com>
2025-05-29 01:07:58 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.env.DEV
-		? "http://localhost:5000/api"
-		: "https://mern-ecom-backend-oay9.onrender.com/api",
+	baseURL: "https://mern-ecom-backend-oay9.onrender.com/api",
 	withCredentials: true,
 });
 
